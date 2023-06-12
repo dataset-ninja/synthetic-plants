5421034361c2e971ec02a8e2fe8543ff87983069
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-06-12 17:56:05 +0300

    For autoupdate.

--- src/main.py
+++ src/main.py
@@ -1,238 +1,73 @@
+import argparse
 import json
 import os
+import sys
 
-import dataset_tools as dtools
 import supervisely as sly
+from dataset_tools import ProjectRepo
 from dotenv import load_dotenv
 
+import src.settings as s
 from src.convert import convert_and_upload_supervisely_project
 
-# !    Checklist before running the app:
-# * 1. Set project name and project name full.
-# * 2. Prepare convert_and_upload_supervisely_project() function in convert.py
-#      It should receive API object, workspace_id and project_name and return project_info of the created project.
-# * 3. Fill out neccessary fields in custom data dict.
-# * 4. Launch the script.
-
-# * Names of the project that will appear on instance and on Ninja webpage.
-PROJECT_NAME = "Synthetic plants"
-PROJECT_NAME_FULL = "Synthetic RGB-D data for plant segmentation"
-
-# * Create instance of supervisely API object.
+PARENT_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+LOCAL_ENV = os.path.join(PARENT_PATH, "local.env")
 load_dotenv(os.path.expanduser("~/ninja.env"))
-load_dotenv("local.env")
-api = sly.Api.from_env()
-team_id = sly.env.team_id()
-workspace_id = sly.env.workspace_id()
-server_address = os.getenv("SERVER_ADDRESS")
-sly.logger.info(
-    f"Connected to Supervisely. Server address: {server_address}, team_id: {team_id}, workspace_id: {workspace_id}."
-)
-
-# * Create directories for result stats and visualizations.
-os.makedirs("./stats/", exist_ok=True)
-os.makedirs("./visualizations/", exist_ok=True)
-
-# * Trying to retreive project info from instance by name.
-project_info = api.project.get_info_by_name(workspace_id, PROJECT_NAME)
-if not project_info:
-    # * If project doesn't found on instance, create it and use new project info.
-    project_info = convert_and_upload_supervisely_project(api, workspace_id, PROJECT_NAME)
-    sly.logger.info(f"Project {PROJECT_NAME} not found on instance. Created new project.")
-else:
-    sly.logger.info(f"Found project {PROJECT_NAME} on instance, will use it.")
-
-project_id = project_info.id
-
-# * How the app will work: from instance or from local directory.
-from_instance = True  # ToDo: Automatically detect if app is running from instance or locally.
-
-# * Step 1: Read project and project meta
-# ? Option 1: From supervisely instance
-
-if from_instance:
-    sly.logger.info("The app in the instance mode. Will download data from Supervisely.")
-
-    project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
-    datasets = api.dataset.get_list(project_id)
-
-    sly.logger.info(
-        f"Prepared project meta and read {len(datasets)} datasets for project with id={project_id}."
-    )
-
-# ? Option 2: From local directory
-# ! Not implemented yet
-# project_path = os.environ["LOCAL_DATA_DIR"]
-# sly.download(api, project_id, project_path, save_image_info=True, save_images=False)
-# project_meta = sly.Project(project_path, sly.OpenMode.READ).meta
-# datasets = None
-
-# * Step 2: Get download link
-download_sly_url = dtools.prepare_download_link(project_info)
-dtools.update_sly_url_dict({project_id: download_sly_url})
-sly.logger.info(f"Prepared download link: {download_sly_url}")
+load_dotenv(LOCAL_ENV)
+SERVER_ADDRESS = os.getenv("SERVER_ADDRESS")
+TEAM_ID = sly.env.team_id()
+WORKSPACE_ID = sly.env.workspace_id()
 
 
-# * Step 3: Update project custom data
-sly.logger.info("Updating project custom data...")
-custom_data = {
-    #####################
-    # ! required fields #
-    #####################
-    "name": PROJECT_NAME,
-    "fullname": PROJECT_NAME_FULL,
-    "cv_tasks": ["semantic segmentation", "instance segmentation"],
-    "annotation_types": ["semantic segmentation", "instance segmentation"],
-    "industries": ["general domain"],  # ! Not sure
-    "release_year": 2018,
-    "homepage_url": "https://www.kaggle.com/datasets/kumaresanmanickavelu/lyft-udacity-challenge",
-    "license": "CC0: Public Domain",
-    "license_url": "https://creativecommons.org/publicdomain/zero/1.0/",
-    "preview_image_id": 250014,  # ! To fill after upload
-    "github_url": "https://github.com/dataset-ninja/synthetic-plants",
-    "github": "dataset-ninja/synthetic-plants",
-    "citation_url": None,  # ! Not sure
-    "download_sly_url": download_sly_url,
-    #####################
-    # ? optional fields #
-    #####################
-    "download_original_url": "https://www.kaggle.com/datasets/harlequeen/synthetic-rgbd-images-of-plants/download?datasetVersionNumber=3",
-    # "paper": None,
-    # "organization_name": None,
-    # "organization_url": None,
-    # "tags": [],
-}
+def get_project_info(api: sly.Api):
+    s.check_names()
 
-# * Update custom data and retrieve updated project info and custom data from instance.
-api.project.update_custom_data(project_id, custom_data)
-project_info = api.project.get_info_by_id(project_id)
-custom_data = project_info.custom_data
-sly.logger.info("Successfully updated project custom data.")
+    project_info = api.project.get_info_by_name(WORKSPACE_ID, s.PROJECT_NAME)
+    if not project_info:
+        # If project doesn't found on instance, create it and use new project info.
+        project_info = convert_and_upload_supervisely_project(api, WORKSPACE_ID, s.PROJECT_NAME)
+        sly.logger.info(f"Project {s.PROJECT_NAME} not found on instance. Created new project.")
+        sly.logger.info("Now you can explore created project and choose 'preview_image_id'.")
+        sys.exit(0)
+    else:
+        sly.logger.info(f"Found project {s.PROJECT_NAME} on instance, will use it.")
 
-
-def build_stats():
-    sly.logger.info("Starting to build stats...")
-
-    stats = [
-        dtools.ClassBalance(project_meta),
-        dtools.ClassCooccurrence(project_meta, force=False),
-        dtools.ClassesPerImage(project_meta, datasets),
-        dtools.ObjectsDistribution(project_meta),
-        dtools.ObjectSizes(project_meta),
-        dtools.ClassSizes(project_meta),
-    ]
-    heatmaps = dtools.ClassesHeatmaps(project_meta)
-    classes_previews = dtools.ClassesPreview(project_meta, project_info.name, force=False)
-    previews = dtools.Previews(project_id, project_meta, api, team_id)
-
-    for stat in stats:
-        if not sly.fs.file_exists(f"./stats/{stat.basename_stem}.json"):
-            stat.force = True
-    stats = [stat for stat in stats if stat.force]
-
-    if not sly.fs.file_exists(f"./stats/{heatmaps.basename_stem}.png"):
-        heatmaps.force = True
-    if not sly.fs.file_exists(f"./visualizations/{classes_previews.basename_stem}.webm"):
-        classes_previews.force = True
-    if not api.file.dir_exists(team_id, f"/dataset/{project_id}/renders/"):
-        previews.force = True
-    vstats = [stat for stat in [heatmaps, classes_previews, previews] if stat.force]
-
-    dtools.count_stats(
-        project_id,
-        stats=stats + vstats,
-        sample_rate=1,
-    )
-
-    sly.logger.info("Saving stats...")
-    for stat in stats:
-        with open(f"./stats/{stat.basename_stem}.json", "w") as f:
-            json.dump(stat.to_json(), f)
-        stat.to_image(f"./stats/{stat.basename_stem}.png")
-
-    if len(vstats) > 0:
-        if heatmaps.force:
-            heatmaps.to_image(f"./stats/{heatmaps.basename_stem}.png", draw_style="outside_black")
-        if classes_previews.force:
-            classes_previews.animate(f"./visualizations/{classes_previews.basename_stem}.webm")
-        if previews.force:
-            previews.close()
-
-    sly.logger.info("Successfully built and saved stats.")
-
-
-def build_visualizations():
-    sly.logger.info("Starting to build visualizations...")
-
-    renderers = [
-        dtools.Poster(project_id, project_meta, force=False),
-        dtools.SideAnnotationsGrid(project_id, project_meta),
-    ]
-    animators = [
-        dtools.HorizontalGrid(project_id, project_meta),
-        dtools.VerticalGrid(project_id, project_meta, force=False),
-    ]
-
-    for vis in renderers + animators:
-        if not sly.fs.file_exists(f"./visualizations/{vis.basename_stem}.png"):
-            vis.force = True
-    renderers, animators = [r for r in renderers if r.force], [a for a in animators if a.force]
-
-    for a in animators:
-        if not sly.fs.file_exists(f"./visualizations/{a.basename_stem}.webm"):
-            a.force = True
-    animators = [a for a in animators if a.force]
-
-    # ? Download fonts from: https://fonts.google.com/specimen/Fira+Sans
-    dtools.prepare_renders(
-        project_id,
-        renderers=renderers + animators,
-        sample_cnt=40,
-    )
-
-    sly.logger.info("Saving visualizations...")
-
-    for vis in renderers + animators:
-        vis.to_image(f"./visualizations/{vis.basename_stem}.png")
-    for a in animators:
-        a.animate(f"./visualizations/{a.basename_stem}.webm")
-
-    sly.logger.info("Successfully built and saved visualizations.")
-
-
-def build_summary():
-    sly.logger.info("Starting to build summary...")
-
-    summary_data = dtools.get_summary_data_sly(project_info)
-
-    classes_preview = None
-    if sly.fs.file_exists("./visualizations/classes_preview.webm"):
-        classes_preview = (
-            f"{custom_data['github_url']}/raw/main/visualizations/classes_preview.webm"
-        )
-
-    summary_content = dtools.generate_summary_content(
-        summary_data,
-        vis_url=classes_preview,
-    )
-
-    with open("SUMMARY.md", "w") as summary_file:
-        summary_file.write(summary_content)
-
-    sly.logger.info("Successfully built and saved summary.")
-
-
-def main():
-    pass
-
-    sly.logger.info("Script is starting...")
-
-    build_stats()
-    build_visualizations()
-    build_summary()
-
-    sly.logger.info("Script finished successfully.")
+    return project_info
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Upload dataset to instance.")
+    parser.add_argument(
+        "--forces", type=json.loads, default="{}", help="Which parameters to force."
+    )
+
+    args = parser.parse_args()
+    forces = args.forces
+
+    sly.logger.info(f"Script is starting with forces: {forces}")
+
+    sly.fs.mkdir("./stats")
+    sly.fs.mkdir("./visualizations")
+
+    api = sly.Api.from_env()
+    sly.logger.info(
+        f"Connected to Supervisely. Server address: {SERVER_ADDRESS}, team_id: {TEAM_ID}, workspace_id: {WORKSPACE_ID}."
+    )
+    project_id = get_project_info(api).id
+    settings = s.get_settings()
+
+    sly.logger.info(f"Starting to work with project id: {project_id}.")
+
+    force_stats = forces.get("force_stats")
+    force_visuals = forces.get("force_visuals")
+    force_texts = forces.get("force_texts")
+
+    project_repo = ProjectRepo(api, project_id, settings)
+    project_repo.build_stats(force=force_stats)
+    project_repo.build_visualizations(force=force_visuals)
+
+    # * Optional parameter for preview_class should be passed if needed:
+    # * Literal["ClassesPreview", "HorizontalGrid", "SideAnnotationsGrid"]
+    project_repo.build_texts(force=force_texts)
+
+    sly.logger.info("Script finished.")
---
+++ src/settings.py
@@ -0,0 +1,82 @@
+from typing import Dict, List, Optional, Union
+
+from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+
+##################################
+# * Before uploading to instance #
+##################################
+PROJECT_NAME: str = "Synthetic plants"
+PROJECT_NAME_FULL: str = "Synthetic RGB-D data for plant segmentation"
+
+##################################
+# * After uploading to instance ##
+##################################
+LICENSE: License = License.CC0_1_0()
+INDUSTRIES: List[Industry] = [Industry.Agriculture()]
+CV_TASKS: List[CVTask] = [CVTask.SemanticSegmentation(), CVTask.InstanceSegmentation()]
+ANNOTATION_TYPES: List[AnnotationType] = [
+    AnnotationType.SemanticSegmentation(),
+    AnnotationType.InstanceSegmentation(),
+]
+
+RELEASE_YEAR: int = 2018
+HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/kumaresanmanickavelu/lyft-udacity-challenge"
+# e.g. "https://some.com/dataset/homepage"
+
+PREVIEW_IMAGE_ID: int = 250014
+# This should be filled AFTER uploading images to instance, just ID of any image.
+
+GITHUB_URL: str = "https://github.com/dataset-ninja/synthetic-plants"
+# URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
+
+##################################
+### * Optional after uploading ###
+##################################
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+# Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
+
+CLASS2COLOR: Optional[Dict[str, List[str]]] = None
+# If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
+
+PAPER: Optional[str] = None
+CITATION_URL: Optional[str] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+TAGS: List[str] = None
+
+##################################
+###### ? Checks. Do not edit #####
+##################################
+
+
+def check_names():
+    fields_before_upload = [PROJECT_NAME, PROJECT_NAME_FULL]
+    if any([field is None for field in fields_before_upload]):
+        raise ValueError("Please fill all fields in settings.py before uploading to instance.")
+
+
+def get_settings():
+    settings = {
+        "project_name": PROJECT_NAME,
+        "project_name_full": PROJECT_NAME_FULL,
+        "license": LICENSE,
+        "industries": INDUSTRIES,
+        "cv_tasks": CV_TASKS,
+        "annotation_types": ANNOTATION_TYPES,
+        "release_year": RELEASE_YEAR,
+        "homepage_url": HOMEPAGE_URL,
+        "preview_image_id": PREVIEW_IMAGE_ID,
+        "github_url": GITHUB_URL,
+    }
+    if any([field is None for field in settings.values()]):
+        raise ValueError("Please fill all fields in settings.py after uploading to instance.")
+
+    settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
+    settings["class2color"] = CLASS2COLOR
+    settings["paper"] = PAPER
+    settings["citation_url"] = CITATION_URL
+    settings["organization_name"] = ORGANIZATION_NAME
+    settings["organization_url"] = ORGANIZATION_URL
+    settings["tags"] = TAGS if TAGS is not None else []
+
+    return settings
